10be7df54cb8dfd812414e78d19500589b95325c
VABorisov <vasiliy.a.github@gmail.com>
2023-07-01 22:48:22 +0300

    dataset_caltech256_results

--- 1_task/dataset_caltech256.py
+++ 1_task/dataset_caltech256.py
@@ -213,3 +213,31 @@ if __name__ == "__main__":
 
     plt.ioff()
     plt.show()
+    
+    def visualize_model_predictions(model,img_path):
+        was_training = model.training
+        model.eval()
+
+        img = Image.open(img_path)
+        img = data_transforms['val'](img)
+        img = img.unsqueeze(0)
+        img = img.to(device)
+
+        with torch.no_grad():
+            outputs = model(img)
+            _, preds = torch.max(outputs, 1)
+
+            ax = plt.subplot(2,2,1)
+            ax.axis('off')
+            ax.set_title(f'Predicted: {class_names[preds[0]]}')
+            imshow(img.cpu().data[0])
+
+            model.train(mode=was_training)
+        
+    visualize_model_predictions(
+    model_conv,
+    img_path=r'E:\datasets\archive\val\001.ak47\001_0083.jpg'
+    )
+
+    plt.ioff()
+    plt.show()
