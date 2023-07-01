a099300471857954883228de79537c79b92d09e6
VABorisov <vasiliy.a.github@gmail.com>
2023-07-01 13:57:00 +0300

    result_finetuning_model_from_example

--- 1_task/example_from_guide.py
+++ 1_task/example_from_guide.py
@@ -185,6 +185,7 @@ if __name__ == "__main__":
     model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                        num_epochs=25)
     
-    print(model_ft)
-    
     visualize_model(model_ft)
+    
+    plt.ioff()
+    plt.show()
